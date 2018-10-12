b71b02a1cd8c08e76f0c5c431bae69eafb35cbd2
Rui Lamy <ruilamy@Ruis-MacBook-Pro.local>
2018-10-12 15:29:17 -0400

    Clean code

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -4,45 +4,10 @@
 # Created by Lee Robinson
 # Modified by Rui Lamy
 
-
 from pygame import *
 import sys
 from os.path import abspath, dirname
 from random import randint, choice
-
-# Configure the module according to your needs
-from datadog import initialize
-
-options = {
-    'api_key':'',
-    'app_key':''
-}
-
-initialize(**options)
-
-# Use Datadog REST API client
-from datadog import api
-
-title = "Something big happened!"
-text = 'And let me tell you all about it here!'
-tags = ['version:1', 'application:web']
-
-api.Event.create(title=title, text=text, tags=tags)
-
-
-# Use Statsd, a Python client for DogStatsd
-from datadog import statsd
-
-# Increment a counter.
-statsd.increment('page.views')
-
-# Or ThreadStats, an alternative tool to collect and flush metrics,using Datadog REST API
-from datadog import ThreadStats
-stats = ThreadStats()
-stats.start()
-stats.increment('page.views')
-
-
 
 BASE_PATH = abspath(dirname(__file__))
 FONT_PATH = BASE_PATH + '/fonts/'
@@ -67,11 +32,6 @@ IMG_NAMES = ['ship', 'mystery',
              'laser', 'enemylaser']
 IMAGES = {name: image.load(IMAGE_PATH + '{}.png'.format(name)).convert_alpha()
           for name in IMG_NAMES}
-
-
-
-
-
 
 class Ship(sprite.Sprite):
     def __init__(self):
